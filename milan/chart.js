596b0fd85cd9b5564a1d953d68f323a403175b4b
Leonardo Donelli <learts92@gmail.com>
2014-07-15 00:55:27 +0200

    Milan: dynamic color scale.

--- milan/chart.js
+++ milan/chart.js
@@ -15,6 +15,15 @@
 		.append('g')
 		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+	function generateColors(luminance=60, croma=60, offset=0) {
+		var colors = [];
+		for (var i = 0; i < 20; i++) {
+			colors.push(d3.hcl((i*18+offset)%360, croma, luminance));
+		}
+		return colors;
+	}
+	colors = generateColors();
+
 	jesus.plot = function(dataset) {
 		var t = d3.map();// {country: 'total', years: []};
 		for (var i = 0; i < 34; i++) {
@@ -42,23 +51,26 @@
 			})));
 		var colorScale = d3.scale.category20c()
 			.domain(dataset.map(function(d) { return d.country; }));
+		var colorScale2 = d3.scale.ordinal()
+			.domain(dataset.map(function(d) { return d.country; }))
+			.range(colors);
 
 		var xAxis = d3.svg.axis()
-			.scale(xScale)
+		.scale(xScale)
 			.tickFormat(function(d) { return "'"+(d+'').substr(-2,2); })
 			.orient('bottom');
 		var yAxis = d3.svg.axis()
 			.scale(yScale)
 			.tickFormat(function(d) { return d*100 + '%'; })
 			.orient('right');
-		
+
 		var area = d3.svg.area()
 			.x(function(d) { return xScale(d.year); })
 			.y0(function(d) { return yScale(d.y0); })
 			.y1(function(d) { return yScale(d.y0+d.y); });
 		var stack = d3.layout.stack()
 			.values(function(d) { return d.years; })
-			.offset('silhouette')
+			.offset('zero')
 			.x(function(d) { return d.year; })
 			.y(function(d) { return d.value/t.get(d.year); });
 		// dataset = [dataset[61]];
@@ -68,7 +80,7 @@
 		series = canvas.selectAll('g').data(stack(dataset)).enter().append('g');
 		series.append('path')
 			.attr('class', 'area')
-			.style('fill', function(d) { return colorScale(d.country); })
+			.style('fill', function(d) { return colorScale2(d.country); })
 		// .style('stroke', function(d) { return colorScale(d.country); })
 			.attr('d', function(d) {
 				return area(d.years);
@@ -94,11 +106,33 @@
 				});
 		}
 
+		var redrawChart = function() {
+			allSeries = canvas.selectAll('g').data(stack(dataset));
+			allSeries.enter().append('g').append('path')
+				.attr('class', 'area')
+				.style('fill', function(d) { return colorScale2(d.country); })
+				.attr('d', function(d) {
+					return area(d.years);
+				});
+			allSeries.selectAll('path')
+				.attr('class', 'area')
+				.style('fill', function(d) { return colorScale2(d.country); })
+				.attr('d', function(d) {
+					return area(d.years);
+				});
+		}
+
 		d3.select('#controls select').on('change', function(d) {
 			stack.offset(this.value);
-			// console.log(this.value);
-			// console.log(d3.select(this)[0].value);
 			updateStackOffset();
+		});
+
+		d3.selectAll('#controls input').on('change', function(d) {
+			l = +d3.select('#luminance').property('value');
+			c = +d3.select('#chroma').property('value');
+			o = +d3.select('#offset').property('value');
+			colorScale2.range(generateColors(l, c, o));
+			redrawChart();
 		});
 
 		// d3.selectAll('path.area').on('mouseover', function(d, i) {
