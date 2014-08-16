a0af40b5ad2f9c5a8945eaa7fa950658cae7fc65
Leonardo Donelli <learts92@gmail.com>
2014-08-16 18:36:54 +0200

    Add colors, ignore AutoModerator, do not draw self linking chords and other small improvements.

--- reddit-moderators/chart.js
+++ reddit-moderators/chart.js
@@ -27,6 +27,9 @@
 		'politics',
 		'mildlyinteresting',
 	];
+	var palette = ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99",
+	               "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a",
+	               "#ffff99", "#b15928"];
 
 	var svg = d3.select('#chart_area').append('svg')
 		.attr('width', 1000)
@@ -45,7 +48,7 @@
 			return row.map(function(cell) {
 				return cell.length;
 			});
-		}));
+		})).sortSubgroups(d3.descending);
 	}
 
 	charter.init = function(dataset) {
@@ -57,13 +60,15 @@
 			matrix[i] = new Array(charter.subreddits.length);
 			for (var j = 0; j < charter.subreddits.length; j++) {
 				matrix[i][j] = [];
+				if (i === j) { matrix[i][j] = new Array(20); }
 			}
 		}
 		users.filter(function(d) { return d.values.length > 1; })
 			.forEach(function(d) {
+				if (d.key === 'AutoModerator') { return; }
 				for (var i = 0; i < d.values.length; i++) {
+					var s1 = charter.subreddits.indexOf(d.values[i].subreddit);
 					for (var j = i+1; j < d.values.length; j++) {
-						var s1 = charter.subreddits.indexOf(d.values[i].subreddit);
 						var s2 = charter.subreddits.indexOf(d.values[j].subreddit);
 						matrix[s1][s2].push(d.values[0].name);
 						matrix[s2][s1].push(d.values[0].name);
@@ -72,19 +77,23 @@
 			});
 		setLayoutParams();
 		console.log(chordLayout.matrix());
+		var cc = chordLayout.chords().filter(function(chord) {
+			return chord.target.index !== chord.source.index;
+		});
 		svg.append('g')
 			.selectAll('g')
 			.data(chordLayout.groups)
 			.enter()
 			.append('path')
-			.attr('d', function(d) { return arcGenerator(d); });
+			.attr('d', function(d) { return arcGenerator(d); })
+			.style('fill', function(d,i) { return palette[i%12]; });
 		svg.append('g').attr('class', 'chord')
 			.selectAll('.path')
-			.data(chordLayout.chords).enter()
+			.data(cc).enter()
 			.append('path')
-			.attr('d', chordGenerator)
-			.attr('stroke', 'blue')
-			.style('fill', 'red');
+			.attr('fill', function(d) { return palette[d.source.index]; })
+			.attr('opacity', 0.8)
+			.attr('d', chordGenerator);
 		console.log(chordLayout.chords());
 	}
 
